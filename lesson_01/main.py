f5254234e52f1fa465c617ae30ef7bc07979f5b1
Dilmurod Rakhmatullaev <dilmurod.rakhmatullaev.dev@gmail.com>
2026-04-25 23:01:16 +0300

    Add CRUD operations for books API

--- lesson_01/main.py
+++ lesson_01/main.py
@@ -1,5 +1,6 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -17,10 +18,39 @@ books = [
 ]
 
 
-@app.get("/books")
+@app.get("/books",
+         tags=["📚 books"],
+         summary="Get all books"
+)
 def read_books():
     return books
 
+
+@app.get("/books/{book_id}",
+         tags=["📚 books"],
+         summary="Get book by id"
+)
+def get_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    raise HTTPException(status_code=404, detail="Book not found")
+
+
+class NewBook(BaseModel):
+    title: str
+    author: str
+
+@app.post("/books",
+          tags=["📚 books"],
+          summary="Create a new book",)
+def create_book(new_book: NewBook):
+    books.append({
+        "id": len(books) + 1,
+        "title": new_book.title,
+        "author": new_book.author
+    })
+    return {"success": True, "message": "Book created"}
 
 
 if __name__ == "__main__":
